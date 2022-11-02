cce0f3b8c98a9e48dc773a9d3958ae57867a4617
y-seongho <117188785+y-seongho@users.noreply.github.com>
2022-11-02 13:02:34 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -73,3 +73,25 @@ function renderQuote() {
             $('#author').text(author)
         })
 }
+
+var Target = document.getElementById("clock");
+var Target_apm = document.getElementById("apm");
+    function clock() {
+        var time = new Date();
+        var hours = time.getHours();
+        var minutes = time.getMinutes();
+        var seconds = time.getSeconds();
+        var AmPm ="AM";
+        if(hours > 12){   
+            var AmPm ="PM";
+            hours %= 12;
+        }
+
+        Target.innerText = 
+        `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+
+        Target_apm.innerText = `${AmPm}`;
+
+    }
+clock();
+setInterval(clock, 1000); // 1초마다 실행
